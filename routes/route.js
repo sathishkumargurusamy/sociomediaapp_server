fc5c0ca6e3b1a58bcde854448fb5ac36cb1a8adc
sathishkumargurusamy <gsathishkumar2597@gmail.com>
2020-02-13 17:11:56 +0530

    Intialising server

--- routes/route.js
+++ routes/route.js
@@ -303,9 +303,9 @@ router.post('/likes', (req, res, next) => {
         }
     });
 });
-router.delete('/likes/:body', (req, res, next) => {
+router.delete('/likes/:uid&:pid', (req, res, next) => {
     pusher.trigger('likes', 'likes-removed', 'Likes deleted');
-    likes.remove({ userid: req.params.body.userid, postid: req.prams.body._id }, function(err, result) {
+    likes.remove({ userid: req.params.uid, postid: req.params.pid }, function(err, result) {
         if (err) {
             res.json(err);
         } else {
