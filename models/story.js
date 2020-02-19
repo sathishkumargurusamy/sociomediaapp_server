2b12624bfd40ec37dc486f02549c3565d4ac024c
sathishkumargurusamy <gsathishkumar2597@gmail.com>
2020-02-19 15:51:19 +0530

    Updated expire time for story

--- models/story.js
+++ models/story.js
@@ -17,7 +17,7 @@ const Storyschema = mongoose.Schema({
     expireAt: {
         type: Date,
         default: Date.now,
-        index: { expires: '60m' },
+        index: { expires: '50m' },
     }
 });
 
